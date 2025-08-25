d0b14ad0709f8b8a52ec3fdcf15e5dd5a271c055
Lucas <asd@asd.com>
2025-08-25 19:48:19 -0300

    tercer commit

--- entrega2.py
+++
@@ -1,9 +0,0 @@
-from proyectopaq.mi_modulo import Cliente
-
-def main():
-    cliente1 = Cliente("Paul", "McCartney", 44, 1)
-    print(cliente1)
-    cliente1.mostrar_info()
-    cliente1.servicio_pago()
-
-main()
